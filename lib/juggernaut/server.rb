a664dea74f073f7597fdd6c6e7ac7fe51046d990
Szymon Nowak <szimek@gmail.com>
2009-02-03 00:09:14 +0100

    Added new 'query' command to add client to channel

--- lib/juggernaut/server.rb
+++ lib/juggernaut/server.rb
@@ -8,7 +8,7 @@ require 'digest/sha1'
 module Juggernaut
   class Server < EventMachine::Connection
     include Juggernaut::Miscel
-    
+
     class InvalidRequest < Juggernaut::JuggernautError #:nodoc:
     end
 
@@ -45,7 +45,7 @@ module Juggernaut
     POLICY_REQUEST = "<policy-file-request/>"
 
     CR              = "\0"
-    
+
     attr_reader   :current_msg_id
     attr_reader   :messages
     attr_reader   :connected
@@ -55,7 +55,7 @@ module Juggernaut
     attr_reader   :client
 
     # EM methods
-    
+
     def post_init
       logger.debug "New client [#{client_ip}]"
       @client         = nil
@@ -65,7 +65,7 @@ module Juggernaut
       @logout_timeout = nil
       @buffer = ''
     end
-    
+
     # Juggernaut packets are terminated with "\0"
     # so we need to buffer the data until we find the
     # terminating "\0"
@@ -73,7 +73,7 @@ module Juggernaut
       @buffer << data
       @buffer = process_whole_messages(@buffer)
     end
-    
+
     # process any whole messages in the buffer,
     # and return the new contents of the buffer
     def process_whole_messages(data)
@@ -88,46 +88,46 @@ module Juggernaut
       messages.each {|message| process_message(message.strip)}
       return data
     end
-    
+
     def process_message(ln)
       logger.debug "Processing message: #{ln}"
       @request        = nil
-      
+
       if ln == POLICY_REQUEST
         logger.debug "Sending crossdomain file"
         send_data POLICY_FILE.gsub('PORT', (options[:public_port]||options[:port]).to_s)
         close_connection_after_writing
         return
       end
-      
+
       begin
         @request = JSON.parse(ln) unless ln.empty?
       rescue
         raise CorruptJSON, ln
       end
-      
+
       raise InvalidRequest, ln if !@request
-      
+
       @request.symbolize_keys!
-      
+
       # For debugging
       @request[:ip] = client_ip
-      
+
       @request[:channels] = (@request[:channels] || []).compact.select {|c| !!c && c != '' }.uniq
-      
-      if @request[:client_ids] 
+
+      if @request[:client_ids]
         @request[:client_ids] = @request[:client_ids].to_a.compact.select {|c| !!c && c != '' }.uniq
       end
-      
+
       case @request[:command].to_sym
-        when :broadcast: broadcast_command
-        when :subscribe: subscribe_command
-        when :query:     query_command
-        when :noop:      noop_command
+        when :broadcast then broadcast_command
+        when :subscribe then subscribe_command
+        when :query     then query_command
+        when :noop      then noop_command
       else
         raise InvalidCommand, @request
       end
-    
+
     rescue JuggernautError => e
       logger.error("#{e} - #{e.message.inspect}")
       close_connection
@@ -135,7 +135,7 @@ module Juggernaut
     rescue => e
       logger ? logger.error(e) : puts(e)
     end
-    
+
     def unbind
       if @client
         # todo - should be called after timeout?
@@ -144,74 +144,74 @@ module Juggernaut
       end
       mark_dead('Unbind called')
     end
-    
+
     # As far as I'm aware, send_data
     # never throws an exception
     def publish(msg)
       logger.debug "Sending msg: #{msg.to_s} to client #{@request[:client_id]} (session #{@request[:session_id]})"
       send_data(msg.to_s + CR)
     end
-    
+
     # Connection methods
-    
+
     def broadcast(bdy)
       msg = Juggernaut::Message.new(@current_msg_id += 1, bdy, self.signature)
       publish(msg)
     end
-    
+
     def mark_dead(reason = "Unknown error")
       # Once dead, a client never recovers since a reconnection
       # attempt would hook onto a new em instance. A client
-      # usually dies through an unbind 
+      # usually dies through an unbind
       @connected = false
       @client.remove_connection(self) if @client
     end
-    
+
     def alive?
       @connected == true
     end
-    
+
     def has_channels?(channels)
       channels.each {|channel|
         return true if has_channel?(channel)
       }
       false
     end
-    
+
     def has_channel?(channel)
       @channels.include?(channel)
     end
-    
+
     def add_channel(chan_name)
       return if !chan_name or chan_name == ''
       @channels << chan_name unless has_channel?(chan_name)
     end
-    
+
     def add_channels(chan_names)
       chan_names.to_a.each do |chan_name|
         add_channel(chan_name)
       end
     end
-    
+
     def remove_channel!(chan_name)
       @channels.delete(chan_name)
     end
-    
+
     def remove_channels!(chan_names)
       chan_names.to_a.each do |chan_name|
         remove_channel!(chan_name)
       end
     end
-    
+
     protected
-    
+
       # Commands
-    
+
       def broadcast_command
         raise MalformedBroadcast, @request unless @request[:type]
 
         raise UnauthorisedBroadcast, @request unless authenticate_broadcast_or_query
-        
+
         case @request[:type].to_sym
           when :to_channels
             # if channels is a blank array, sends to everybody!
@@ -226,23 +226,30 @@ module Juggernaut
           raise MalformedBroadcast, @request
         end
       end
-      
+
       def query_command
         raise MalformedQuery, @request unless @request[:type]
-              
+
         raise UnauthorisedQuery, @request unless authenticate_broadcast_or_query
-              
+
         case @request[:type].to_sym
           when :remove_channels_from_all_clients
             query_needs :channels
             clients = Juggernaut::Client.find_all
             clients.each {|client| client.remove_channels!(@request[:channels]) }
           when :remove_channels_from_client
+            query_needs :client_id, :channels
+            client = Juggernaut::Client.find_by_id(@request[:client_id])
+            client.remove_channels!(@request[:channels]) if client
+          when :remove_channels_from_clients
             query_needs :client_ids, :channels
             @request[:client_ids].each do |client_id|
               client = Juggernaut::Client.find_by_id(client_id)
               client.remove_channels!(@request[:channels]) if client
             end
+          when :add_channels_to_client
+            query_needs :channels
+            add_channels(@request[:channels]) if @request[:channels]
           when :show_channels_for_client
             query_needs :client_id
             if client = Juggernaut::Client.find_by_id(@request[:client_id])
@@ -267,62 +274,62 @@ module Juggernaut
           raise MalformedQuery, @request
         end
       end
-    
+
       def noop_command
         logger.debug "NOOP"
       end
-    
+
       def subscribe_command
         logger.debug "SUBSCRIBE: #{@request.inspect}"
-        
+
         if channels = @request[:channels]
           add_channels(channels)
         end
-        
+
         @client = Juggernaut::Client.find_or_create(self, @request)
-        
+
         if !@client.subscription_request(@channels)
           raise UnauthorisedSubscription, @client
         end
-        
+
         if options[:store_messages]
           @client.send_queued_messages(self)
         end
       end
-    
+
     private
-    
+
       # Different broadcast types
-    
+
       def broadcast_to_channels(msg, channels = [])
         Juggernaut::Client.find_all.each {|client| client.send_message(msg, channels) }
       end
-      
+
       def broadcast_to_client(body, client_id, channels)
         client = Juggernaut::Client.find_by_id(client_id)
         client.send_message(body, channels) if client
       end
-      
+
       # Helper methods
-      
+
       def broadcast_needs(*args)
         args.each do |arg|
           raise MalformedBroadcast, @request unless @request.has_key?(arg)
         end
       end
-      
+
       def subscribe_needs(*args)
         args.each do |arg|
           raise MalformedSubscribe, @request unless @request.has_key?(arg)
         end
       end
-      
+
       def query_needs(*args)
         args.each do |arg|
           raise MalformedQuery, @request unless @request.has_key?(arg)
         end
       end
-      
+
       def authenticate_broadcast_or_query
         if options[:allowed_ips]
           return true if options[:allowed_ips].include?(client_ip)
@@ -336,7 +343,7 @@ module Juggernaut
         end
         false
       end
-      
+
       def broadcast_query_request
         return false unless options[:broadcast_query_login_url]
         url = URI.parse(options[:broadcast_query_login_url])
@@ -356,7 +363,7 @@ module Juggernaut
         end
         true
       end
-      
+
       def client_ip
         Socket.unpack_sockaddr_in(get_peername)[1]
       end
--- test/test_server.rb
+++ test/test_server.rb
@@ -6,9 +6,9 @@ require "shoulda"
 require "mocha"
 
 class TestServer < Test::Unit::TestCase
-  
+
   CONFIG = File.join(File.dirname(__FILE__), "files", "juggernaut.yml")
-  
+
   DEFAULT_OPTIONS = {
     :host => "0.0.0.0",
     :port => 5001,
@@ -17,9 +17,9 @@ class TestServer < Test::Unit::TestCase
     :timeout => 10,
     :store_messages => false
   }
-  
+
   OPTIONS = DEFAULT_OPTIONS.merge(YAML::load(ERB.new(IO.read(CONFIG)).result))
-  
+
   class DirectClient
     attr_reader :channels
     def broadcast_to_channels(channels, body)
@@ -51,8 +51,16 @@ class TestServer < Test::Unit::TestCase
       self.transmit :command => :query, :type => :remove_channels_from_all_clients, :channels => channels
       self
     end
-    def query_remove_channels_from_client(channels, clients)
-      self.transmit :command => :query, :type => :remove_channels_from_client, :client_ids => clients, :channels => channels
+    def query_remove_channels_from_client(channels, client)
+      self.transmit :command => :query, :type => :remove_channels_from_client, :client_id => client, :channels => channels
+      self
+    end
+    def query_remove_channels_from_clients(channels, clients)
+      self.transmit :command => :query, :type => :remove_channels_from_clients, :client_ids => clients, :channels => channels
+      self
+    end
+    def query_add_channels_to_client(channels)
+      self.transmit :command => :query, :type => :add_channels_to_client, :channels => channels
       self
     end
     def query_show_channels_for_client(client_id)
@@ -111,7 +119,7 @@ class TestServer < Test::Unit::TestCase
       self.send_raw(hash.to_json, wait_response)
     end
   end
-  
+
   # Assert that the DirectClient has an awaiting message with +body+.
   def assert_body(body, subscriber)
     assert_response subscriber do |result|
@@ -119,21 +127,21 @@ class TestServer < Test::Unit::TestCase
       assert_equal body, result["body"]
     end
   end
-  
+
   # Assert that the DirectClient has no awaiting message.
   def assert_no_body(subscriber)
     assert_response subscriber do |result|
       assert_equal false, result
     end
   end
-  
+
   def assert_no_response(subscriber)
     assert_not_nil subscriber
     assert_raise(EOFError) { subscriber.receive }
   ensure
     subscriber.close
   end
-  
+
   def assert_raw_response(subscriber, response = nil)
     assert_not_nil subscriber
     result = nil
@@ -147,7 +155,7 @@ class TestServer < Test::Unit::TestCase
   ensure
     subscriber.close
   end
-  
+
   def assert_response(subscriber, response = nil)
     assert_not_nil subscriber
     result = nil
@@ -161,12 +169,12 @@ class TestServer < Test::Unit::TestCase
   ensure
     subscriber.close
   end
-  
+
   def assert_server_disconnected(subscriber)
     assert_not_nil subscriber
     assert_raise(Errno::ECONNRESET, EOFError) { subscriber.receive }
   end
-  
+
   # Convenience method to create a new DirectClient instance with overridable options.
   # If a block is passed, control is yielded, passing the new client in. This method
   # returns the value returned from that block, or the new client if no block was given.
@@ -178,7 +186,7 @@ class TestServer < Test::Unit::TestCase
       c
     end
   end
-  
+
   # Shortcut to run tests that require setting up, starting, then shutting down EventMachine.
   # So ugly, but EventMachine doesn't have test examples on code that require back-and-forth
   # communication over a long-running connection.
@@ -189,20 +197,20 @@ class TestServer < Test::Unit::TestCase
     # Save the current options. This is an obvious hack.
     old_options, Juggernaut.options = Juggernaut.options, OPTIONS.merge(options)
     Juggernaut.logger.level = Logger::DEBUG
-    
+
     # Initialize an array to keep track of connections made to the server in this instance.
     @connections = [ ]
-    
+
     EM.run do
       # Start the server, and save each connection made so we can refer to it later.
       EM.start_server(Juggernaut.options[:host], Juggernaut.options[:port], Juggernaut::Server) { |c| @connections << c }
-      
+
       # Guard against never-ending tests by shutting off at 2 seconds.
       EM.add_timer(2) do
         Juggernaut::Client.send_logouts_to_all_clients
         EM.stop
       end
-      
+
       # Deferred: evaluate the block and then run the shutdown proc. By using instance_eval,
       # our block gets access to assert_* methods and the +@connections+ variable above.
       EM.defer proc {
@@ -221,9 +229,9 @@ class TestServer < Test::Unit::TestCase
     # Restore old options.
     Juggernaut.options = old_options if old_options
   end
-  
+
   context "Server" do
-    
+
     should "accept a connection" do
       with_server do
         self.new_client do |c|
@@ -234,7 +242,7 @@ class TestServer < Test::Unit::TestCase
       end
       assert_equal false, @connections.first.alive?
     end
-    
+
     should "register channels correctly" do
       with_server do
         self.new_client { |c| c.transmit :command => :subscribe, :channels => ["master", "slave"] }
@@ -244,11 +252,11 @@ class TestServer < Test::Unit::TestCase
       assert @connections.first.has_channels?(["non_existant", "master", "slave"])
       assert_equal false, @connections.first.has_channels?(["non_existant", "invalid"])
     end
-    
+
     context "channel-wide broadcast" do
-      
+
       body = "This is a channel-wide broadcast test!"
-      
+
       should "be received by client in the same channel" do
         subscriber = nil
         with_server do
@@ -261,7 +269,7 @@ class TestServer < Test::Unit::TestCase
         assert_respond_to result, :[]
         assert_equal body, result["body"]
       end
-      
+
       should "not be received by client not in a channel" do
         subscriber = nil
         with_server do
@@ -270,7 +278,7 @@ class TestServer < Test::Unit::TestCase
         end
         assert_no_response subscriber
       end
-      
+
       should "not be received by client in a different channel" do
         subscriber = nil
         with_server do
@@ -279,14 +287,14 @@ class TestServer < Test::Unit::TestCase
         end
         assert_no_response subscriber
       end
-      
+
     end
-    
+
     # For some reason, these refuse to pass:
     context "broadcast with no specific channel" do
-      
+
       body = "This is a broadcast test!"
-      
+
       should "be received by client not in any channels" do
         subscriber = nil
         with_server do
@@ -295,7 +303,7 @@ class TestServer < Test::Unit::TestCase
         end
         assert_body body, subscriber
       end
-      
+
       should "be received by client in a channel" do
         subscriber = nil
         with_server do
@@ -304,13 +312,13 @@ class TestServer < Test::Unit::TestCase
         end
         assert_body body, subscriber
       end
-      
+
     end
-    
+
     context "broadcast to a client" do
-      
+
       body = "This is a client-specific broadcast test!"
-      
+
       should "be received by the target client" do
         subscriber = nil
         with_server do
@@ -319,7 +327,7 @@ class TestServer < Test::Unit::TestCase
         end
         assert_body body, subscriber
       end
-      
+
       should "not be received by other clients" do
         subscriber = nil
         with_server do
@@ -328,7 +336,7 @@ class TestServer < Test::Unit::TestCase
         end
         assert_no_response subscriber
       end
-      
+
       should "be saved until the client reconnects" do
         subscriber = nil
         with_server :store_messages => true do
@@ -343,18 +351,18 @@ class TestServer < Test::Unit::TestCase
         old_subscriber = nil
         new_subscriber = nil
 
-        with_server :store_messages => true, :timeout => 30 do 
+        with_server :store_messages => true, :timeout => 30 do
           old_subscriber = self.new_client(:client_id => "broadcast_client", :session_id => "1") { |c| c.subscribe %w() }
           self.new_client { |c| c.broadcast_to_clients %w(broadcast_client), body }
           @connections.first.client.expects(:send_message_to_connection).times(2)
           new_subscriber = self.new_client(:client_id => "broadcast_client", :session_id => "2") { |c| c.subscribe %w() }
         end
       end
-      
+
     end
-    
+
     context "querying client list" do
-      
+
       should "return all clients" do
         subscriber = nil
         with_server do
@@ -368,7 +376,7 @@ class TestServer < Test::Unit::TestCase
         assert_equal 3, result.size
         assert_same_elements %w(alex bob cindy), result.collect { |r| r["client_id"] }
       end
-      
+
       should "not include disconnected clients" do
         subscriber = nil
         with_server(:timeout => 0) do
@@ -382,7 +390,7 @@ class TestServer < Test::Unit::TestCase
         assert_equal 2, result.size
         assert_same_elements %w(sandra vivian), result.collect { |r| r["client_id"] }
       end
-      
+
       should "only return requested clients" do
         subscriber = nil
         with_server do
@@ -397,7 +405,7 @@ class TestServer < Test::Unit::TestCase
         assert_equal 2, result.size
         assert_same_elements %w(dixie fanny), result.collect { |r| r["client_id"] }
       end
-      
+
       should "never return non-existant clients even when requested" do
         subscriber = nil
         with_server do
@@ -411,7 +419,7 @@ class TestServer < Test::Unit::TestCase
         assert_not_nil result
         assert_equal 0, result.size
       end
-      
+
       should "return correct number of active connections" do
         subscriber = nil
         with_server do
@@ -424,7 +432,7 @@ class TestServer < Test::Unit::TestCase
         assert_equal 1, result.size
         assert_equal 5, result.first["num_connections"]
       end
-      
+
       should "be equivalent when querying one client" do
         s1, s2 = nil
         with_server do
@@ -441,7 +449,7 @@ class TestServer < Test::Unit::TestCase
         assert_equal 1, r2.size
         assert_equal r1, r2.first
       end
-      
+
       should "only return clients in specific channels" do
         subscriber = nil
         with_server do
@@ -458,11 +466,11 @@ class TestServer < Test::Unit::TestCase
           assert_same_elements %w(alexa bobby cindy dixon geoff), result.collect { |r| r["client_id"] }
         end
       end
-      
+
     end
-    
+
     context "upon processing an invalid command" do
-      
+
       should "disconnect immediately" do
         subscriber = nil
         with_server do
@@ -470,13 +478,13 @@ class TestServer < Test::Unit::TestCase
         end
         assert_server_disconnected subscriber
       end
-      
+
     end
-    
+
     %w(broadcast subscribe query).each do |type|
-      
+
       context "upon receiving malformed #{type}" do
-        
+
         should "disconnect immediately" do
           subscriber = nil
           with_server do
@@ -484,13 +492,13 @@ class TestServer < Test::Unit::TestCase
           end
           assert_server_disconnected subscriber
         end
-        
+
       end
-      
+
     end
-    
+
     context "upon receiving invalid JSON" do
-      
+
       should "disconnect immediately" do
         subscriber = nil
         with_server do
@@ -498,11 +506,11 @@ class TestServer < Test::Unit::TestCase
         end
         assert_server_disconnected subscriber
       end
-      
+
     end
-    
+
     context "crossdomain file request" do
-      
+
       should "return contents of crossdomain file" do
         subscriber = nil
         with_server do
@@ -514,11 +522,11 @@ class TestServer < Test::Unit::TestCase
       </cross-domain-policy>
     EOF
       end
-      
+
     end
-    
+
     context "querying channel list" do
-      
+
       should "return channel list" do
         subscribe = nil
         with_server do
@@ -530,15 +538,15 @@ class TestServer < Test::Unit::TestCase
           }
         end
         assert_response subscribe do |result|
-          assert_equal 3, result.size 
+          assert_equal 3, result.size
           assert_same_elements %w(master slave1 slave2), result
         end
       end
-      
+
     end
-    
-    context "remove channel request" do
-      
+
+    context "remove channels request" do
+
       should "work on all clients when requested" do
         with_server do
           self.new_client(:client_id => "homer") { |c| c.subscribe %w(groupie master slave1 slave2) }
@@ -552,22 +560,52 @@ class TestServer < Test::Unit::TestCase
           assert_does_not_contain connection.channels, /slave/
         end
       end
-      
+
       should "work on specific clients when requested" do
         with_server do
           self.new_client(:client_id => "homer") { |c| c.subscribe %w(groupie master slave1 slave2) }
           self.new_client(:client_id => "marge") { |c| c.subscribe %w(master slave1 slave2) }
           self.new_client(:client_id => "pinocchio") { |c|
             c.subscribe %w(master slave1 slave2)
-            c.query_remove_channels_from_client %w(slave1 slave2), %w(homer)
+            c.query_remove_channels_from_clients %w(slave1 slave2), %w(homer pinocchio)
+          }
+        end
+        assert_does_not_contain @connections.find { |c| c.instance_eval("@request[:client_id]") == "homer" }.channels, /slave/
+        assert_does_not_contain @connections.find { |c| c.instance_eval("@request[:client_id]") == "pinocchio" }.channels, /slave/
+        assert_contains @connections.find { |c| c.instance_eval("@request[:client_id]") == "marge" }.channels, /slave/
+      end
+
+      should "work on specific client when requested" do
+        with_server do
+          self.new_client(:client_id => "homer") { |c| c.subscribe %w(groupie master slave1 slave2) }
+          self.new_client(:client_id => "marge") { |c| c.subscribe %w(master slave1 slave2) }
+          self.new_client(:client_id => "pinocchio") { |c|
+            c.subscribe %w(master slave1 slave2)
+            c.query_remove_channels_from_client %w(slave1 slave2), "homer"
           }
         end
         assert_does_not_contain @connections.find { |c| c.instance_eval("@request[:client_id]") == "homer" }.channels, /slave/
         assert_contains @connections.find { |c| c.instance_eval("@request[:client_id]") == "marge" }.channels, /slave/
       end
-      
+
     end
-    
+
+    context "add channels request" do
+
+      should "work on specific client when requested" do
+        with_server do
+          self.new_client(:client_id => "homer") { |c| c.subscribe %w(groupie) }
+          self.new_client(:client_id => "pinocchio") { |c|
+            c.subscribe %w(groupie)
+            c.query_add_channels_to_client %w(slave1 slave2)
+          }
+        end
+        assert_contains @connections.find { |c| c.instance_eval("@request[:client_id]") == "pinocchio" }.channels, /slave/
+        assert_does_not_contain @connections.find { |c| c.instance_eval("@request[:client_id]") == "homer" }.channels, /slave/
+      end
+
+    end
+
   end
-  
+
 end
